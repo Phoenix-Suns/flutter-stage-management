650dad820bd00892e64504e893245936cc1af363
Nghia Tesuji <nghia@tesuji.io>
2023-02-22 09:32:14 +0700

    add avatar url

--- lib/mvvm_provider/movie.dart
+++ lib/mvvm_provider/movie.dart
@@ -5,6 +5,6 @@ class Movie {
   Movie({required this.title, required this.poster});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(title: json["full_name"], poster: "");
+    return Movie(title: json["full_name"], poster: json["owner"]["avatar_url"]);
   }
 }
